ecf1781f5b23c731d2c3a1b41848084bf41e7a88
saumel verma <saumel2001@gmail.com>
2021-06-12 22:17:01 +0530

    Added some details to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error) {
             callback('Unable to find location! Enter a valid location',undefined)
         } else{
-            callback(undefined,'Weather: ' + body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature + ' degree Celsius out. But it feels like '+ body.current.feelslike + ' degree Celsius')
+            callback(undefined,'Weather: ' + body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature + ' degree Celsius out. But it feels like '+ body.current.feelslike + ' degree Celsius out.' + 'The humidity is '+ body.current.humidity + '%.')
         }
     })
 }
